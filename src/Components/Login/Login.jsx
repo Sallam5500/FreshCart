b2e1b1cbec96b332e0c725d81b65d98d2e971f52
Sallam5500 <mohamedsallammmmm550@gmail.com>
2025-02-11 11:05:13 +0200

    text

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -56,7 +56,7 @@ const formik = useFormik({
     <h2>Login</h2>
 
 <form onSubmit={formik.handleSubmit} className="md:w-1/2 mx-auto">
-{apiError &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50  dark:text-red-400" role="alert">
+{apiError &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50 dark:text-red-400" role="alert">
 {apiError}
 </div>}
   
@@ -65,7 +65,7 @@ const formik = useFormik({
     <input type="email" name="email" id="email" value={formik.values.email}  onChange={formik.handleChange} onBlur={formik.handleBlur} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-main-500 focus:outline-none focus:ring-0 focus:border-main-600 peer" placeholder=""  />
     <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-main-600 peer-focus:dark:text-main-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter your Email</label>
   </div>
-  {formik.errors.email &&formik.touched.email &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50  dark:text-red-400" role="alert">
+  {formik.errors.email &&formik.touched.email &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50 dark:text-red-400" role="alert">
 {formik.errors.email}
 </div>}
 
@@ -73,7 +73,7 @@ const formik = useFormik({
       <input type="password" name="password" id="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-main-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-main-500 focus:outline-none focus:ring-0 focus:border-main-600 peer" placeholder="" />
       <label for="password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-main-600 peer-focus:dark:text-main-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter your Password</label>
   </div>
-  {formik.errors.password &&formik.touched.password &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50  dark:text-red-400" role="alert">
+  {formik.errors.password &&formik.touched.password &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50 dark:text-red-400" role="alert">
 {formik.errors.password}
 </div>}
  
