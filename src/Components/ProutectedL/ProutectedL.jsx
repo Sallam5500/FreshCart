e38d669f31ac311926feb0fbdec2a351d1ee9bb6
Sallam5500 <mohamedsallammmmm550@gmail.com>
2025-02-11 10:41:25 +0200

    Route

--- src/Components/ProutectedL/ProutectedL.jsx
+++ src/Components/ProutectedL/ProutectedL.jsx
@@ -3,9 +3,9 @@ import { Navigate } from 'react-router-dom'
 
 export default function ProutectedL({children}) {
   if (localStorage.getItem('userToken')) {
-    return children
+    return <Navigate to={'/home'}/>
   }else{
-    return <Navigate to={'/login'}/>
+    return children
   }
 
 
