72c22972e6883b0db377ecc9cec7b0527ad63b75
Sallam5500 <mohamedsallammmmm550@gmail.com>
2025-02-11 14:54:39 +0200

    dark mood

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -85,7 +85,7 @@ const formik = useFormik({
 {apiError}
 </div>}
   <div className="relative z-0 w-full mb-5 group">
-    <input type="text" name="name" id="name" value={formik.values.name}  onChange={formik.handleChange} onBlur={formik.handleBlur} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-main-500 focus:outline-none focus:ring-0 focus:border-main-600 peer" placeholder=" "  />
+    <input type="text" name="name" id="name" value={formik.values.name}  onChange={formik.handleChange} onBlur={formik.handleBlur} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none  dark:border-gray-600 dark:focus:border-main-500 focus:outline-none focus:ring-0 focus:border-main-600 peer" placeholder=" "  />
     <label htmlFor="name" className="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-main-600 peer-focus:dark:text-main-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Enter your Name</label>
   </div>
   {formik.errors.name &&formik.touched.name &&   <div className="px-4 py-4 mb-5 text-sm text-red-800 rounded-lg bg-red-50  dark:text-red-400" role="alert">
